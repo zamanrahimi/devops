6cf4d5b6baf91b33d5ac1e970d982a53addbb1bb
Rahimi <Zaman.Rahimi@energytransfer.com>
2024-02-21 15:08:53 -0600

    docker-compose

--- php/project_root/app/index.php
+++ php/project_root/app/index.php
@@ -1,3 +1,19 @@
-<h1> This is a simple php page!</h1>
-<h2>Third heading is added</h2>
-<h2>Fourht heading is added</h2>
+<?php
+$host = 'db';  // This is the service name of your MySQL container
+$db = 'energytransfer';
+$user = 'root';  // Replace with your MySQL username
+$pass = 'admin';  // Replace with your MySQL password
+
+// Create a connection
+$conn = new mysqli($host, $user, $pass, $db);
+
+// Check the connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+echo "Connected to MySQL successfully!";
+
+// Close the connection
+$conn->close();
+?>
--- php/project_root/ops/README.md
+++ php/project_root/ops/README.md
@@ -1,2 +1,2 @@
-1. docker build -t zamanrahimi1368/php_app:v1.4 .
-2. docker push zamanrahimi1368/php_app:v1.4
+1. docker build -t zamanrahimi1368/php_app:v1.6 .
+2. docker push zamanrahimi1368/php_app:v1.6
