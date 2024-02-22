f00472fe9b0136941837cab69c47535e982288c8
Rahimi <Zaman.Rahimi@energytransfer.com>
2024-02-22 14:58:25 -0600

    PV and PVC is created

--- php/project_root/ops/README.md
+++
@@ -1,2 +0,0 @@
-1. docker build -t zamanrahimi1368/php_app:v1.6 .
-2. docker push zamanrahimi1368/php_app:v1.6
---
+++ php/project_root/php/index.php
@@ -0,0 +1,36 @@
+<?php
+$servername = "mysql-service";  // The name of the MySQL service in your Kubernetes cluster
+$username = "root";
+$password = "";
+$database = "db2";
+$port = 3306;
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database, $port);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+echo "Connected successfully";
+
+// Perform your database operations here
+
+// Fetch data from the "empt" table
+$sql = "SELECT id, name FROM empt";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // Output data of each row
+    echo "<ul>";
+    while($row = $result->fetch_assoc()) {
+        echo "<li>ID: " . $row["id"]. " - Name: " . $row["name"]. "</li>";
+    }
+    echo "</ul>";
+} else {
+    echo "No records found";
+}
+
+$conn->close();
+?>
