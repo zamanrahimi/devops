69da723898bcccc4dc3b16e3f508b3a7cb335a2f
Rahimi <Zaman.Rahimi@energytransfer.com>
2024-02-23 15:41:28 -0600

    Current application version: v2.18

--- php/project_root/app/index.php
+++ php/project_root/app/index.php
@@ -1,4 +1,4 @@
-<h2>Version: v2.17</h2>
+<h2>Version: v2.18</h2>
 <?php
 $servername = "mysql-service";  // The name of the MySQL service in your Kubernetes cluster
 $username = "root";
--- php/project_root/public/commands/base64_converter.py
+++ php/project_root/public/commands/base64_converter.py
@@ -1,12 +1,5 @@
 import base64
 
-# Replace these values with your actual MySQL root password and database name
-mysql_root_password = "your_mysql_password"
-mysql_database_name = "db2"
-
-# Encode the values to base64
-encoded_root_password = base64.b64encode(mysql_root_password.encode()).decode()
-encoded_database_name = base64.b64encode(mysql_database_name.encode()).decode()
-
-print(f"Encoded Root Password: {encoded_root_password}")
-print(f"Encoded Database Name: {encoded_database_name}")
+database_name = "db2"
+encoded_database_name = base64.b64encode(database_name.encode()).decode()
+print(f"Base64 Encoded Database Name: {encoded_database_name}")
