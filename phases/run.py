8e0fe34d6fc361d87afd6041c6fad24234869dd2
Rahimi <Zaman.Rahimi@energytransfer.com>
2024-02-29 13:17:09 -0600

    DevOps phase 1

---
+++ phases/run.py
@@ -0,0 +1,55 @@
+import webbrowser
+
+
+
+
+
+
+# 1. phase one, planning 
+# 2. phase two, coding 
+# 3. phase three, build 
+# 4. phase four, test 
+# 5. phase five, release
+# 6. phase six, deploy
+# 7. phase seven, operate
+# 8. phase eight, monitor
+
+
+
+
+
+
+
+import subprocess as sb
+
+# import color module
+import component.public.colors as color
+commands = [
+    
+    # a_plan 
+    "python a_plan/plan.py",
+
+    # c_build / repo 
+    "python c_build/repo/repo.py",
+
+]
+
+for command in commands:
+    try:
+        # execute main command
+        sb.run(command, check=True, shell=True)
+        print(
+            f"{color.Colors.BLUE}----------Command {command} ---------- {color.Colors.RESET}"
+        )
+
+        print(
+            f" {color.Colors.GREEN} ( {command} ) exected successfully {color.Colors.RESET} \n"
+        )
+
+    except sb.CalledProcessError as e:
+        print(
+            f"{color.Colors.RED}----------------Command {command} ------------------------ {color.Colors.RESET}"
+        )
+        print(
+            f" {color.Colors.RED} The error for command ( {command} ) is {e} {color.Colors.RESET}"
+        )
